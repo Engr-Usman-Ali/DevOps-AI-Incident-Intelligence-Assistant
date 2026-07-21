58f6ba535c1ef2fd10257a37d12d3eda6fdb898c
Engr-Usman-Ali <usmanali08675@gmail.com>
2026-07-21 13:21:50 +0500

    Upload Raw env

--- backend/app/ai/groq_client.py
+++ backend/app/ai/groq_client.py
@@ -6,3 +6,19 @@ from app.core.config import GROQ_API_KEY
 client = Groq(
     api_key=GROQ_API_KEY,
 )
+
+if __name__ == "__main__":
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": "Say Hello",
+            }
+        ],
+    )
+
+    print(
+        response.choices[0].message.content
+    )
