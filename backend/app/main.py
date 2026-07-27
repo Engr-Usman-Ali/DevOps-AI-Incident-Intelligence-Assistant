5289db2862459195366e93021d91b85fa3bc26f6
Engr-Usman-Ali <usmanali08675@gmail.com>
2026-07-27 15:31:10 +0500

    Update Vercel URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ Base.metadata.create_all(bind=engine)
 # CORS Configuration
 origins = [
     "http://localhost:5173",
-    "https://dev-ops-ai-incident-intelligence-assistant-8ws4jylyx.vercel.app",
+    "https://dev-ops-ai-incident-intelligence-as.vercel.app/",
 ]
 
 app.add_middleware(
